cc0fdaa1b5696fbe7e982aeed23d5c2f09a78698
sungtaek <leesungtaek@gmail.com>
2024-09-04 07:50:20 +0000

    indent

--- server/src/main/java/io/lazybones/speeco/SpeecoApplication.java
+++ server/src/main/java/io/lazybones/speeco/SpeecoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpeecoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpeecoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpeecoApplication.class, args);
+  }
 
 }

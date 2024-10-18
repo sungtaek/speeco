3f7faf11b18045cbfb44f6eb292bd1e075411ff7
sungtaek <leesungtaek@gmail.com>
2024-10-18 18:53:09 +0900

    asr use pcm16

--- server/src/main/java/io/lazybones/speeco/asr/whisper/Whisper.java
+++ server/src/main/java/io/lazybones/speeco/asr/whisper/Whisper.java
@@ -1,5 +1,9 @@
 package io.lazybones.speeco.asr.whisper;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
@@ -33,7 +37,9 @@ public class Whisper implements ASR {
         .next()
         .flatMap(mergedAudio -> {
           MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
-          bodyBuilder.part("audio_file", new ByteArrayResource(mergedAudio))
+          ByteArrayOutputStream bodyOutputStream = new ByteArrayOutputStream();
+          writeAsWave(bodyOutputStream, 1, 16000, 16000, mergedAudio);
+          bodyBuilder.part("audio_file", new ByteArrayResource(bodyOutputStream.toByteArray()))
               .contentType(MediaType.valueOf("audio/wave"))
               .filename("sample.wav");
           log.info("Send to ASR: {} bytes", mergedAudio.length);
@@ -54,5 +60,52 @@ public class Whisper implements ASR {
         })
         .doOnNext(s -> log.info("Receive from ASR: {}", s));
   }
-  
+
+  private void writeAsWave(OutputStream output, int channelCount, int sampleRate, int bitsPerSample, byte[] data) {
+    try {
+      // WAVE RIFF header
+      writeToOutput(output, "RIFF"); // chunk id
+      writeToOutput(output, 36 + data.length); // chunk size
+      writeToOutput(output, "WAVE"); // format
+
+      // SUB CHUNK 1 (FORMAT)
+      writeToOutput(output, "fmt "); // subchunk 1 id
+      writeToOutput(output, 16); // subchunk 1 size
+      writeToOutput(output, (short) 1); // audio format (1 = PCM)
+      writeToOutput(output, (short) channelCount); // number of channelCount
+      writeToOutput(output, sampleRate); // sample rate
+      writeToOutput(output, sampleRate * channelCount * bitsPerSample / 8); // byte rate
+      writeToOutput(output, (short) (channelCount * bitsPerSample / 8)); // block align
+      writeToOutput(output, (short) bitsPerSample); // bits per sample
+
+      // SUB CHUNK 2 (AUDIO DATA)
+      writeToOutput(output, "data"); // subchunk 2 id
+      writeToOutput(output, data.length); // subchunk 2 size
+      writeToOutput(output, data); // audio data
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  private void writeToOutput(OutputStream output, String data) throws IOException {
+    for (int i = 0; i < data.length(); i++)
+      output.write(data.charAt(i));
+  }
+
+  private void writeToOutput(OutputStream output, int data) throws IOException {
+    output.write(data >> 0);
+    output.write(data >> 8);
+    output.write(data >> 16);
+    output.write(data >> 24);
+  }
+
+  private void writeToOutput(OutputStream output, short data) throws IOException {
+    output.write(data >> 0);
+    output.write(data >> 8);
+  }
+
+  private void writeToOutput(OutputStream output, byte[] data) throws IOException {
+    output.write(data, 0, data.length);
+  }
+
 }
